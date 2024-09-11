b0c402e51e41ecff6fb785fd2d28a15e648a1ae5
jefri agistar <jefri4876@gmail.com>
2024-09-11 23:36:57 +0700

    feat: create feature dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './components/Layout';
 import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
 
 function App() {
 	return (
 		<>
 			<Layout>
 				<Header />
+				<Dashboard />
 			</Layout>
 		</>
 	);
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = (props) => {
 	return (
-		<section className='w-full min-h-screen px-8 py-10 lg:py-28 bg-blue-gray-400'>
+		<section className='w-full min-h-screen px-8 py-10 lg:py-28 bg-brown-500'>
 			<div className='w-full mx-auto'>{props.children}</div>
 		</section>
 	);
